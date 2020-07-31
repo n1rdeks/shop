e06e4bcce310b96fa85268fc4246beed8527fa0d
n1rdeks <n1rdeks@mail.ru>
2020-07-31 20:47:37 +0300

    added Error handling and Middleware on API

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Infrastructure.Data;
 using Core.Interfaces;
 using AutoMapper;
 using API.Helpers;
+using API.Middleware;
 
 namespace API
 {
@@ -18,7 +18,7 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-        }
+       }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -35,10 +35,9 @@ namespace API
         // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
