24bf41c518087d145cc7ba012f7ebaf78ebf0a6c
n1rdeks <n1rdeks@mail.ru>
2020-08-05 22:49:31 +0300

    added CORS service

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime.
@@ -44,6 +49,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
