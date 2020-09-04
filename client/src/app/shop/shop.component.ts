37ca86e5ded757339bdd9c8da5ef8c7b76e8506e
n1rdeks <n1rdeks@mail.ru>
2020-09-04 17:27:51 +0300

    added sort and filter

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduct} from '../shared/models/product';
 import {ShopService} from './shop.service';
+import { IProductBrand } from '../shared/models/productBrand';
+import { IProductType } from '../shared/models/productType';
 
 
 @Component({
@@ -10,15 +12,62 @@ import {ShopService} from './shop.service';
 })
 export class ShopComponent implements OnInit {
     products: IProduct[];
+    brands: IProductBrand[];
+    types: IProductType[];
+    brandIdSelected = 0;
+    typeIdSelected = 0;
+    sortSelected = 'name';
+    sortOptions = [
+        {name: 'Alphabetical', value: 'name'},
+        {name: 'Low to High', value: 'priceAsc'},
+        {name: 'High to Low', value: 'priceDesc'},
+    ];
 
     constructor(private shopService: ShopService) { }
 
     ngOnInit(): void {
-        this.shopService.getProducts().subscribe(response => {
-            this.products = response.data;
+        this.getProducts();
+        this.getBrands();
+        this.getTypes();
+    }
+
+    getProducts() {
+        this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected)
+            .subscribe(response => {
+                this.products = response.data;
+            }, error => {
+                console.log(error);
+            });
+    }
+
+    getBrands() {
+        this.shopService.getBrands().subscribe(response => {
+            this.brands = [{id: 0, name: 'All'}, ...response];
         }, error => {
             console.log(error);
         });
     }
 
+    getTypes() {
+        this.shopService.getTypes().subscribe(response => {
+           this.types = [{id: 0, name: 'All'}, ...response];
+        }, error => {
+            console.log(error);
+        });
+    }
+
+    onBrandSelected(brandId: number) {
+        this.brandIdSelected = brandId;
+        this.getProducts();
+    }
+
+    onTypeSelected(typeId: number) {
+        this.typeIdSelected = typeId;
+        this.getProducts();
+    }
+
+    onSortSelected(sort: string) {
+        this.sortSelected  = sort;
+        this.getProducts();
+    }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {IPagination} from '../shared/models/pagination';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { IProductBrand } from '../shared/models/productBrand';
+import { IPagination } from '../shared/models/pagination';
+import { IProductType } from '../shared/models/productType';
+
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ShopService {
     baseUrl = 'https://localhost:5001/api/';
 
     constructor(private http: HttpClient) { }
 
-    getProducts(): Observable<IPagination> {
-        return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+    getProducts(brandId?: number, typeId?: number, sort?: string) {
+        let params = new HttpParams();
+
+        if (brandId) {
+            params = params.append('brandId', brandId.toString());
+        }
+
+        if (typeId) {
+            params = params.append('typeId', typeId.toString());
+        }
+
+        if (sort) {
+            params = params.append('sort', sort);
+        }
+
+        return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
+            .pipe(
+                map(response => {
+                    return response.body;
+                })
+            );
+    }
+
+    getBrands() {
+        return this.http.get<IProductBrand[]>(this.baseUrl + 'products/brands');
+    }
+
+    getTypes() {
+        return this.http.get<IProductType[]>(this.baseUrl + 'products/types');
     }
 }
