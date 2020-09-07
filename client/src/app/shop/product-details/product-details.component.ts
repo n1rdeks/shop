abbca533d58b89e3fbdc7f5c32ea9d77643d8ba2
n1rdeks <n1rdeks@mail.ru>
2020-09-07 15:08:50 +0300

    added new style for bootstrap, added breadcramb, fix scss for animated buttons

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../shared/models/product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
     product: IProduct;
 
-    constructor(private  shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+    constructor(private  shopService: ShopService, private activatedRoute: ActivatedRoute,
+                private bcService: BreadcrumbService) { }
 
     ngOnInit(): void {
         this.loadProduct();
@@ -22,6 +24,7 @@ export class ProductDetailsComponent implements OnInit {
         this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id'))
             .subscribe(product => {
                 this.product = product;
+                this.bcService.set('@productDetails', product.name);
             }, error => {
                 console.log(error);
             });
