8768860794b7b14a694d792f2e352fa18e64ced3
n1rdeks <n1rdeks@mail.ru>
2020-09-12 15:59:48 +0300

    added auth config in Swagger - holy crap, use Postman, Luke

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -11,6 +11,29 @@ namespace API.Extensions
             services.AddSwaggerGen(conf =>
             {
                 conf.SwaggerDoc("v1", new OpenApiInfo {Title = "Shop API", Version = "v1"});
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                conf.AddSecurityDefinition("Bearer", securityScheme);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    {securityScheme, new[] {"Bearer"}}
+                };
+
+                conf.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
