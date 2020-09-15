a0fe148770a161f921b57dc84ea37049ade0717f
n1rdeks <n1rdeks@mail.ru>
2020-09-16 00:18:24 +0300

    added about component, fix style

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { AuthGuard } from './core/guards/auth.guard';
+import { AboutComponent } from './core/about/about.component';
 
 
 const routes: Routes = [
     {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
     {path: 'test-errors', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
+    {path: 'about', component: AboutComponent, data: {breadcrumb: 'About'}},
     {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Errors'}},
     {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
     {path: 'shop', loadChildren: () => import('./shop/shop.module')
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -9,12 +9,13 @@ import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
 import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
     declarations: [
         NavBarComponent, TestErrorComponent, NotFoundComponent,
-        ServerErrorComponent, SectionHeaderComponent
+        ServerErrorComponent, SectionHeaderComponent, AboutComponent
     ],
     imports: [
         CommonModule,
